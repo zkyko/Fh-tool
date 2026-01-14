4b7b6618ceea38df60dd287d93db52c136283ad0
zkyko <nischalbhandari73@gmail.com>
2026-01-14 14:40:24 -0600

    Fix baseUrl configuration for GitHub Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,9 +12,9 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docs-site.com',
+  url: 'https://zkyko.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  baseUrl: '/',
+  baseUrl: '/Fh-tool/',
 
   organizationName: 'fourhands',
   projectName: 'axis-docs',
